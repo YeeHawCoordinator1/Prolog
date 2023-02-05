865b8274db3a20088f3aab0f212026932bc0e204
Lyna028 <Kettanilyna@gmail.com>
2023-02-05 10:23:14 -0500

    fnll

--- index.js
+++ index.js
@@ -60,6 +60,7 @@ let Books = {
       ("war", "biography", "diary", "world war II", "teenage")
     ],
   ],
+  paths : ["achilles.html", " ", "illiad.html", "circe.html", " ", " "]
 };
 
 var tag; //variable contenant le tag du bouton préssé
@@ -76,21 +77,18 @@ function findBook() {
   for (let i = 0; i < Books.tags.length - 1; i++) {
     intersection = Books.tags[i].filter((x) => totalTags.includes(x));
     if (intersection.length >= 3) {
-      booksRecommandation.push(Books.names[i]);
+      booksRecommandation.push(Books.paths[i]);
     }
   }
+
   return booksRecommandation;
 }
 console.log(findBook());
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 6aaf3b78676275707c9a6aa092e09ddd67711398
-function myFunct() {
+function searchBtn() {
     window.location.href = "livre1.html";
 }
-<<<<<<< HEAD
-=======
 
->>>>>>> 6aaf3b78676275707c9a6aa092e09ddd67711398
+function mafct() {
+    window.location.href = "index.html";
+}
