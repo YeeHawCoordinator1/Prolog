cda5e54d95b74bb04d5ca22d7600540548cb86b0
Tarik Ouazzani Touhami <ouazzanitot@gmail.com>
2023-02-05 14:35:49 +0100

    feat: add book category to search list

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
+const selectedBookCategories = [];
 // wait for the DOM to finish loading before accessing the DOM
 document.addEventListener("DOMContentLoaded", () => {
   const buttonsContainer = document.querySelector("#buttons-container");
   const buttons = buttonsContainer.children;
 
+  const selectedBookCategoriesContainer = document.querySelector(
+    "#selected-book-categories"
+  );
+  const searchButton = document.querySelector("#search-button");
+
   Array.from(buttons).forEach((button) => {
     button.addEventListener("click", () => {
+      const bookCategory = button.textContent;
+      const buttonIndex = selectedBookCategories.indexOf(bookCategory);
+      if (buttonIndex > -1) {
+        selectedBookCategories.splice(buttonIndex, 1);
+      } else {
+        selectedBookCategories.push(bookCategory);
+      }
+
       button.classList.toggle("clicked");
+
+      // clear the container
+      selectedBookCategoriesContainer.innerHTML = "";
+
+      // add the selected book categories to the container
+      if (selectedBookCategories.length > 0) {
+        selectedBookCategories.forEach((bookCategory) => {
+          const bookCategoryElement = document.createElement("div");
+          bookCategoryElement.classList.add("selected-book-category");
+          bookCategoryElement.textContent = bookCategory;
+          selectedBookCategoriesContainer.appendChild(bookCategoryElement);
+        });
+      }
     });
   });
 });
